16cb0480d1f74da745502238afa262b646e1ebbf
Ziv-Peleg-Halperin <zivph05@gmail.com>
2024-01-19 22:36:48 +0200

    Deleted Expression Exception, not needed

--- exceptions.py
+++ exceptions.py
@@ -1,8 +1,3 @@
-class ExpressionException(Exception):
-    def __init__(self, message):
-        self.message = message
-
-
 class SyntaxException(Exception):
     def __init__(self, message):
         self.message = message
