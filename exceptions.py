b9060ff8a3a07903bc3326cca587a5b51276708b
Ziv-Peleg-Halperin <zivph05@gmail.com>
2024-01-21 00:14:51 +0200

    Added doc

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,8 @@
 class SyntaxException(Exception):
     def __init__(self, message):
+        """
+        Syntax exception - raised when the expression is incorrect in the syntax
+        Creates the exception
+        :param message: The correlated message given by the System
+        """
         self.message = message
